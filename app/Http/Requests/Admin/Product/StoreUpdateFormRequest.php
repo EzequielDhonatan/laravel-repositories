59054d73e5f753eca1f6e3d65aea8a125bbb402e
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-08-29 18:52:21 -0300

    Module 2.0 - Product (Updated)

---
+++ app/Http/Requests/Admin/Product/StoreUpdateFormRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Admin\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = $this->segment(3);
+
+        return [
+            
+            /* DADOS DO PRODUTO
+            ================================================== */
+            'category_id'           => 'required',
+            'name'                  => "required|min:3|max:60|unique:products,name,{$id},id",
+            'url'                   => "required|min:3|max:60|unique:products,price,{$id},id",
+            'price'                 => 'required',
+            'description'           => 'max:2000',
+            
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
+Auth::routes();
+
 Route::get('admin', function () {
 
 })->name('admin');
 
-Route::resource('admin/categories', 'Admin\Category\IndexController');
-Route::any('admin/categories/search', 'Admin\Category\IndexController@search')->name('categories.search');
+Route::resource('admin/categories', 'Admin\Category\IndexController'); ## CATEGORY (MODULE 1.0)
+Route::any('admin/categories/search', 'Admin\Category\IndexController@search')->name('categories.search'); ## SEARCH CATEGORY (MODULE 1.0)
+
+Route::resource('admin/products', 'Admin\Product\IndexController'); ## PRODUCT (MODULE 2.0)
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
