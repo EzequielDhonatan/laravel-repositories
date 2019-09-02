9aad43b2bc2262075ea3e5719303cf76c78f5088
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-09-02 09:02:00 -0300

    Module 3.0 - Repositories (Updated)

---
+++ app/Repositories/Core/QueryBuilder/QueryBuilderCategoryRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Core\QueryBuilder;
+
+use App\Repositories\Core\BaseQueryBuilderRepository;
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Models\Admin\Category\Category;
+
+class QueryBuilderCategoryRepository extends BaseQueryBuilderRepository implements CategoryRepositoryInterface
+{
+    protected $table = 'categories';
+}
